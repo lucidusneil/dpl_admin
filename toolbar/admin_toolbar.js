4e2182ca9157356db7797dd12cef2c8a9d4c4d0c
young hahn <yhahn@264833.no-reply.drupal.org>
2009-06-08 00:52:46 +0000

    #483870 by sun: Fix for js namespace compatibility with admin_menu and E^ALL errors.

--- toolbar/admin_toolbar.js
+++ toolbar/admin_toolbar.js
@@ -1,22 +1,21 @@
 // $Id$
 
+Drupal.admin = Drupal.admin || {};
+Drupal.admin.toolbar = Drupal.admin.toolbar || {};
+
+Drupal.admin.toolbar.setActive = function(toolbar_id) {
+  // Show the right toolbar
+  $('#admin-toolbar .depth-1 ul.links').addClass('collapsed');
+  $(toolbar_id).removeClass('collapsed');
+  $('div#admin-toolbar, div#admin-toolbar .depth-1').removeClass('collapsed');
+
+  // Switch link active class to corresponding menu item
+  var link_id = toolbar_id.replace('admin-toolbar', 'admin-link');
+  $('#admin-toolbar .depth-0 ul.links a').removeClass('active');
+  $(link_id).addClass('active');
+}
+
 Drupal.behaviors.admin_toolbar = function(context) {
-
-  Drupal.admin = {
-    'toolbar': {}
-  }
-
-  Drupal.admin.toolbar.setActive = function(toolbar_id) {
-    // Show the right toolbar
-    $('#admin-toolbar .depth-1 ul.links').addClass('collapsed');
-    $(toolbar_id).removeClass('collapsed');
-    $('div#admin-toolbar, div#admin-toolbar .depth-1').removeClass('collapsed');
-
-    // Switch link active class to corresponding menu item
-    var link_id = toolbar_id.replace('admin-toolbar', 'admin-link');
-    $('#admin-toolbar .depth-0 ul.links a').removeClass('active');
-    $(link_id).addClass('active');
-  }
 
   // Primary menus
   $('#admin-toolbar .depth-0 ul.links a:not(.processed)').each(function() {
